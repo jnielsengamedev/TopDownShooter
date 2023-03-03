bb4f4637586f0db1fbfb0cc703db7f272541cfee
ckester143 <24ckester@gmail.com>
2023-03-03 13:51:27 -0800

    Spawning in the map in groups of four

---
+++ Assets/Scripts/TopDownShooter/Controllers/Spawner.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    private int enemyCount;
+    private int waveNumber = 1;
+
+    public float timeBetweenEnemySpawn;
+    public float timeBetweenWaves;
+
+    public Transform[] spawnPoints;
+
+    bool spawningWave;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnEnemyWave(waveNumber));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+
+        if (enemyCount == 0 && !spawningWave)
+        {
+            waveNumber++;
+            StartCoroutine(SpawnEnemyWave(waveNumber));
+        }
+    }
+
+    IEnumerator SpawnEnemyWave(int enemiesToSpawn)
+    {
+        spawningWave = true;
+        yield return new WaitForSeconds(timeBetweenWaves); //We wait here to pause between wave spawning
+        for (int i = 0; i < enemiesToSpawn; i++)
+        {
+            Instantiate(enemyPrefab, spawnPoints[Random.Range(0, spawnPoints.Length)].position, enemyPrefab.transform.rotation);
+            yield return new WaitForSeconds(timeBetweenEnemySpawn); //We wait here to give a bit of time between each enemy spawn
+        }
+        spawningWave = false;
+    }
+}
