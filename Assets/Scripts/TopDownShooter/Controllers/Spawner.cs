624066d2bf0f0f654781d037bfbba37c040bae08
jnielsengamedev <jnielsengamedev@gmail.com>
2023-03-03 13:58:08 -0800

    move spawner to namespace

--- Assets/Scripts/TopDownShooter/Controllers/Spawner.cs
+++ Assets/Scripts/TopDownShooter/Controllers/Spawner.cs
@@ -1,47 +1,50 @@
-using UnityEngine;
 using System.Collections;
+using UnityEngine;
 
-public class Spawner : MonoBehaviour
+namespace TopDownShooter.Controllers
 {
-    public GameObject enemyPrefab;
-    private int enemyCount;
-    private int waveNumber = 1;
-
-    public float timeBetweenEnemySpawn;
-    public float timeBetweenWaves;
-
-    public Transform[] spawnPoints;
-
-    bool spawningWave;
-
-
-    // Start is called before the first frame update
-    void Start()
+    public class Spawner : MonoBehaviour
     {
-        StartCoroutine(SpawnEnemyWave(waveNumber));
-    }
+        public GameObject enemyPrefab;
+        private int enemyCount;
+        private int waveNumber = 1;
 
-    // Update is called once per frame
-    void Update()
-    {
-       
+        public float timeBetweenEnemySpawn;
+        public float timeBetweenWaves;
 
-        if (enemyCount == 0 && !spawningWave)
+        public Transform[] spawnPoints;
+
+        bool spawningWave;
+
+
+        // Start is called before the first frame update
+        void Start()
         {
-            waveNumber++;
             StartCoroutine(SpawnEnemyWave(waveNumber));
         }
-    }
 
-    IEnumerator SpawnEnemyWave(int enemiesToSpawn)
-    {
-        spawningWave = true;
-        yield return new WaitForSeconds(timeBetweenWaves); //We wait here to pause between wave spawning
-        for (int i = 0; i < enemiesToSpawn; i++)
+        // Update is called once per frame
+        void Update()
         {
-            Instantiate(enemyPrefab, spawnPoints[Random.Range(0, spawnPoints.Length)].position, enemyPrefab.transform.rotation);
-            yield return new WaitForSeconds(timeBetweenEnemySpawn); //We wait here to give a bit of time between each enemy spawn
+       
+
+            if (enemyCount == 0 && !spawningWave)
+            {
+                waveNumber++;
+                StartCoroutine(SpawnEnemyWave(waveNumber));
+            }
         }
-        spawningWave = false;
+
+        IEnumerator SpawnEnemyWave(int enemiesToSpawn)
+        {
+            spawningWave = true;
+            yield return new WaitForSeconds(timeBetweenWaves); //We wait here to pause between wave spawning
+            for (int i = 0; i < enemiesToSpawn; i++)
+            {
+                Instantiate(enemyPrefab, spawnPoints[Random.Range(0, spawnPoints.Length)].position, enemyPrefab.transform.rotation);
+                yield return new WaitForSeconds(timeBetweenEnemySpawn); //We wait here to give a bit of time between each enemy spawn
+            }
+            spawningWave = false;
+        }
     }
 }
